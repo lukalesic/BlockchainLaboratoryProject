a8645685b9b77982a0c007e7f1f689ffbe5b57e1
lukalesic <lukalesic20@gmail.com>
2024-03-14 11:57:58 +0100

    Initial Commit

---
+++ BlockchainLV1/BlockchainLV1App.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainLV1App.swift
+//  BlockchainLV1
+//
+//  Created by Luka Lešić on 14.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct BlockchainLV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
